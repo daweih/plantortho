f49430ceecd3007535afa41bbc274b6b63e161b4
dawei huang <daweih@me.com>
2014-10-02 17:13:45 +0800

    add test data: plants pep in fasta format into sample

--- parser/leaf_nodes_species2pep_fa.emf.parser.pl
+++ parser/leaf_nodes_species2pep_fa.emf.parser.pl
@@ -14,22 +14,24 @@ use Data::Dumper;
 use Bio::SeqIO;
 
 my %opts;
-GetOptions(\%opts,'newick_tree:s','species_list:s');
+GetOptions(\%opts,'newick_tree:s','species_list:s', 'pep_dir:s');
 my $usage= <<"USAGE";
 	Program: $0
 	INPUT:
 		-newick_tree			
 		-species_list
+		-pep_dir
 	OUTPUT:
 	
 USAGE
 
 #die $usage unless ($opts{newick_tree});
 #die $usage unless ($opts{species_list});
+#die $usage unless ($opts{pep_dir});
 my $startTime=localtime();
 ################################ Main ##########################################################################
 my $treeio = Bio::TreeIO->new(-format => 'newick',
-								-file   => '../sample/ensembl/web_download/OS05G0113900.genetree.newick.tree');
+								-file   => '../sample/ensembl/web_download/OS05G0113900.genetree.newick.tree');$opts{newick_tree} = "../sample/ensembl/web_download/OS05G0113900.genetree.newick.tree";
 #								-file   => $opts{newick_tree});
 my $accession_list;
 while( my $tree = $treeio->next_tree ) {
@@ -46,8 +48,8 @@ while( my $tree = $treeio->next_tree ) {
 		
 	}
 }
-
-open I, "< binomial_full_abbreviation.oryza.txt";
+open I, "< binomial_full_abbreviation.v22.txt";
+$opts{species_list} = "binomial_full_abbreviation.v22.txt";
 #open I, "< ". $opts{species_list};
 my $species;
 while(<I>){
@@ -56,11 +58,7 @@ while(<I>){
 	$species->{$r[1]} = 1;
 }
 close I;
-foreach my $accession (keys %{$accession_list}){
-	next if(!defined $species->{$accession_list->{$accession}->{species_abr}});
-	print $accession, "\t", $accession_list->{$accession}->{species_abr}, "\n";
-}
-__END__
+
 =cut
 my $ortholog_in = Bio::SeqIO->new(-file => "OS05G0113900.ortholog.fasta" ,
 								-format => 'Fasta');
@@ -73,12 +71,17 @@ while ( my $seq = $ortholog_in->next_seq() ) {
 	}
 }
 =cut
-opendir PEP , "./plants_pep" or die "Cannot open plants_pep: $!";
+$opts{pep_dir} = "../sample/ensembl/release-22/plants/fasta/pep/ortho_OS05G0113900/";
+opendir PEP , $opts{pep_dir} or die "Cannot open plants_pep: $!";
+
 foreach my $file (readdir PEP)
 {
 	next if($file !~ /^.*.pep.all.fa$/);
 	my $check_file_species = 0;
 	foreach my $accession (keys %{$accession_list}){
+
+		next if(!defined $species->{$accession_list->{$accession}->{species_abr}});
+
 		my @species_abr = split //,$accession_list->{$accession}->{species_abr};
 		#Aegilops_tauschii.GCA_000347335.1.22.pep.all.fa
 		#Atau
@@ -89,12 +92,11 @@ foreach my $file (readdir PEP)
 	}
 	if($check_file_species){
 #		print $file, "\n";
-		my $pep = Bio::SeqIO->new(-file => "plants_pep/$file" ,
+		my $pep = Bio::SeqIO->new(-file => $opts{pep_dir}. "/$file" ,
 										-format => 'Fasta');
 		while ( my $seq = $pep->next_seq() ) {
 			foreach my $accession (keys %{$accession_list}){
 				if($seq->id =~ /^($accession).*$/ ){
-#					print $1, "\t", $seq->seq, "\n";
 					$accession_list->{$accession}->{seq} = $seq->seq;
 				}
 			}
@@ -104,62 +106,22 @@ foreach my $file (readdir PEP)
 closedir PEP;
 
 foreach my $accession (keys %{$accession_list}){
+
+	next if(!defined $species->{$accession_list->{$accession}->{species_abr}});
+
 	if(defined $accession_list->{$accession}->{seq}){
-		print "yes\t", $accession_list->{$accession}->{species_abr}, "\t$accession\t", $accession_list->{$accession}->{seq},"\n";
+		print $accession_list->{$accession}->{species_abr}, "\t$accession\t", $accession_list->{$accession}->{seq},"\n";
 	}
 	else{
-		print "no\t", $accession_list->{$accession}->{species_abr}, "\t$accession\n";
 	}
 }
+
 ################################ Main ##########################################################################
 #===============================================================================================================
-__END__
-my $options="-option $opts{option}";
+my $options="-newick_tree $opts{newick_tree} -species_list $opts{species_list} -pep_dir $opts{pep_dir} ";
 my $endTime=localtime();
 my $Program = $1 if($0 =~ /(.*)\.pl/);
 open  LOG,">>$Program\_ProgramRunning.Log";
 print LOG "From \<$startTime\> to \<$endTime\>\tperl $0 $options\n";
 close LOG;
-print Dumper($treeio);
-$VAR1 = bless( {
-                 '_handler' => bless( {
-                                        '_treelevel' => 0,
-                                        'nodetype' => 'Bio::Tree::Node',
-                                        '_root_verbose' => 0,
-                                        'treetype' => 'Bio::Tree::Tree'
-                                      }, 'Bio::TreeIO::TreeEventBuilder' ),
-                 '_file' => 'gene_trees_rio.nh',
-                 '_params' => {
-                                'no_bootstrap_values' => 0,
-                                'no_branch_lengths' => 0,
-                                'no_internal_node_labels' => 0,
-                                'bootstrap_style' => 'traditional',
-                                'order_by' => '',
-                                'newline_each_node' => 0,
-                                'internal_node_id' => 'id'
-                              },
-                 '_root_cleanup_methods' => [
-                                              sub { "DUMMY" }
-                                            ],
-                 '_flush_on_write' => 1,
-                 '_filehandle' => \*Symbol::GEN0,
-                 '_root_verbose' => 0,
-                 '_print_tree_count' => 0
-               }, 'Bio::TreeIO::newick' );
-"Species
-Type
-dN/dS
-Ensembl identifier & gene name
-Compare
-Location
-Target %id
-Query %id"
-
-Amborella trichopoda
-Many-to-many
-n/a
-AMTR_s00057p00215390hypothetical protein
-Region ComparisonAlignment (protein)Alignment (cDNA)Gene Tree (image)
-AmTr_v1.0_scaffold00057:4261624-4262748:1
-82
-78
+__END__
