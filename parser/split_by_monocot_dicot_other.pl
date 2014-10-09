12f53b37daf83fcc5e235cd583bce98568a8c423
dawei huang <daweih@me.com>
2014-10-09 15:42:55 +0800

    fix bug.

--- parser/split_by_monocot_dicot_other.pl
+++ parser/split_by_monocot_dicot_other.pl
@@ -1,5 +1,5 @@
 # Contact     : daweih@me.com
-# Date        : Sep 17, 2014
+# Date        : Thu Oct  9 15:28:46 CST 2014
 # Last Update : 
 # Reference   : 
 # Description : 
@@ -11,22 +11,6 @@ use warnings;
 use Getopt::Long;
 use Bio::SeqIO;
 
-my %opts;
-GetOptions(\%opts,'newick_tree:s','species_list:s', 'pep_dir:s');
-my $usage= <<"USAGE";
-	Program: $0
-	INPUT:
-		-newick_tree			
-		-species_list
-		-pep_dir
-	OUTPUT:
-	
-USAGE
-
-#die $usage unless ($opts{newick_tree});
-#die $usage unless ($opts{species_list});
-#die $usage unless ($opts{pep_dir});
-my $startTime=localtime();
 ################################ Main ##########################################################################
 my $split_by_kind;
 my $kind_column_no = 2;
@@ -34,13 +18,11 @@ my $file_name = $ARGV[0];
 my $file_extension;
 
 open I, "< binomial_full_abbreviation.txt";
-$opts{species_list} = "binomial_full_abbreviation.txt";
-#open I, "< ". $opts{species_list};
 my $species;
 while(<I>){
 	chomp;
 	my @r = split /\t/, $_;
-	$species->{$r[0]} = $r[$kind_column_no];
+	$species->{lc($r[1])} = $r[$kind_column_no];
 }
 close I;
 
@@ -75,9 +57,10 @@ if($ARGV[0] =~ /.*\.([c|t]{1}sv)$/){
 						 -format => 'Fasta');
 	while ( my $seq = $fa->next_seq() ) {
 		foreach my $species_abbrev (keys %{$species}){
+#		print $seq->id, "$species_abbrev\n";
 			if(lc($seq->id) =~ /^.*($species_abbrev).*$/ ){
 #				print $species->{$species_abbrev},"\n";
-				$split_by_kind->{$species->{$species_abbrev}} .= $seq->id ."\n". $seq->seq. "\n";
+				$split_by_kind->{$species->{$species_abbrev}} .= ">". $seq->id ."\n". $seq->seq. "\n";
 			}
 		}
 	}
