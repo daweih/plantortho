6269547e5d710f2dc3412c62f12b7bd21419471d
dawei huang <daweih@me.com>
2014-10-29 13:05:38 +0800

    Add the function of finding predecessor with rank.

--- pipeline/phyml_result/reformat4rio_test/phyloxml_get_predecessor.pl
+++ pipeline/phyml_result/reformat4rio_test/phyloxml_get_predecessor.pl
@@ -12,25 +12,53 @@ use warnings;
 use Bio::TreeIO;
 use Data::Dumper;
 
+open I, "< ../../../parser/binomial_full_abbreviation.v22.txt";
+my $binomial;
+while(<I>){
+	chomp;
+	#oryza_sativa	Osat	monocot
+	my @r = split /\t/,$_;
+	$binomial->{$r[0]} = \@r;
+}
+close I;
+
 my $treeio = Bio::TreeIO->new(-format => 'phyloxml',
 #								-file   => '../../../bin/RIO/bcl_2.xml');
-#								-file   => '../../../parser/species_tree_rio.Plants.xml');
-								-file   => '../../../bin/RIO/ncbi_taxonomy.xml');
+								-file   => '../../../parser/species_tree_rio.Plants.xml');
+#								-file   => '../../../bin/RIO/ncbi_taxonomy.xml');
 while( my $tree = $treeio->next_tree ) {
 #	print Dumper($tree);
 #	print Dumper($tree->get_root_node);
 	foreach my $node ($tree->get_nodes){
 		if( $node->is_Leaf ) {
-			my $predecessor = &predecessor($node, "Oryza sativa");
-			print &get_scientific_name($predecessor), "\n";
-			last;
-#                   print "node is a leaf ... \n";
-#			
-			
+			my $leaf_scientific_name = &get_scientific_name($node);
+			my $leaf_scientific_name_formatted = lc($leaf_scientific_name);
+			   $leaf_scientific_name_formatted =~s/species:\s//;
+			   $leaf_scientific_name_formatted =~s/\s/_/;
+			next if( ! exists $binomial->{$leaf_scientific_name_formatted} );
+		
+			my $predecessor = $node->ancestor;
+			my $ac = $predecessor->annotation();
+			while( ! $ac->get_Annotations("taxonomy") ){
+				$predecessor = $predecessor->ancestor;
+				$ac = $predecessor->annotation();
 
-        }
-        else{
-#        	print "no\n";
+				while( ! $ac->{"_annotation"}->{"taxonomy"}[0]->{"_annotation"}->{"rank"}[0]->{"_annotation"}->{"_text"}[0]->{"value"} ){
+					$predecessor = $predecessor->ancestor;
+					$ac = $predecessor->annotation();
+				}
+#				print $ac->{"_annotation"}->{"taxonomy"}[0]->{"_annotation"}->{"rank"}[0]->{"_annotation"}->{"_text"}[0]->{"value"}, "\n";
+			}
+
+			if($ac->get_Annotations("taxonomy")){
+#				print Dumper($ac);
+				print &get_scientific_name($node), "\t";			
+				print &get_rank($predecessor), "\t";			
+				print &get_scientific_name($predecessor), "\n";			
+			}
+			else{
+				print "No\n";
+			}
         }
 	}
 	
@@ -45,6 +73,10 @@ sub predecessor{
 		my $rank = $annotation->{"_annotation"}->{"rank"}[0]->{"_annotation"}->{"_text"}[0]->{"value"};
 		if($name = $search_string){
 			my $predecessor = $node->ancestor;
+#			$predecessor = $predecessor->ancestor;
+#			$predecessor = $predecessor->ancestor;
+#			$predecessor = $predecessor->ancestor;
+#			print $name, "\t", $rank;
 			return $predecessor;
 		}	
 	}
@@ -56,8 +88,16 @@ sub get_scientific_name{
 	my $ac = $node->annotation();
 	foreach my $annotation ($ac->get_Annotations("taxonomy")){
 		my $name = $annotation->{"_annotation"}->{"scientific_name"}[0]->{"_annotation"}->{"_text"}[0]->{"value"};
+		return $name;
+	}
+}
+sub get_rank{
+	my ($node) = @_;
+
+	my $ac = $node->annotation();
+	foreach my $annotation ($ac->get_Annotations("taxonomy")){
 		my $rank = $annotation->{"_annotation"}->{"rank"}[0]->{"_annotation"}->{"_text"}[0]->{"value"};
-		return $type.": ".$name;
+		return $rank;
 	}
 }
 
