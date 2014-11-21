1c293ec8a4d77ad6a05bb574b2d8c5e07c95664d
yangl <yang0523li@126.com>
2014-11-21 13:42:54 +0800

    to parser the leaf_nodes_species2pep_fa.emf.parser.pl output file

---
+++ parser/filter_newick.parser.out.v2.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl;
+use strict;
+use warnings;
+###############################################################
+# Author: yangl
+# Date: 2014.9.29
+# Description: This program is to get normal .fa file from newick.parser.pl output. 
+###############################################################
+
+my $usage = "\tDescription: get normal .fa file from leaf_nodes_species2pep_fa.emf.parser.pl  output.\n\tUsage: $0 <fastafile>\t<full_index>\t<outfile>\t<output_id_index>\n";
+if(!@ARGV){
+        print STDERR $usage;
+        exit 1;
+}
+
+open IN1,"<","$ARGV[0]";
+open IN2,"<","$ARGV[1]";
+open OUT,">","$ARGV[2]";
+open OUT2,">","$ARGV[3]";
+
+my %hash;
+my $i = 1;
+
+while(<IN2>){
+	chomp;
+	my @fields = split /\t/;
+#	print "$fields[3]\n";
+	my @entry = split (/_/,$fields[3]);
+	my $key =  substr($entry[0],0,1).substr($entry[1],0,3);
+#	print $key . "\n";
+	$hash{$key}=$fields[4];
+}
+
+while(<IN1>){
+	chomp;
+	next if /^no/;
+	my @entry = split /\s/; 
+	print OUT ">$i\n$entry[2]\n";
+	print OUT2 "$i\t$entry[1]_$hash{$entry[0]}\n";
+#	print $hash{$entry[1]} . "\n";
+	$i++;
+}
+close IN1;
+close IN2;
+close OUT;
+close OUT2;
